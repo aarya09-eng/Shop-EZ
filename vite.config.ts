a4883dfcfd9a3551b61253dd486d0fa27aa3afe6
Aarya Dharme <dharmeaarya86@gmail.com>
2026-09-05 12:10:08 +0530

    upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     tanstackStart({ server: { entry: "server" } }),
+    nitro({ preset: "vercel" }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
